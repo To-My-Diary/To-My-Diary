cba9131220b7df6971371bceb1c8f76cca6b1368
wooyong99 <wulovesk@naver.com>
2023-05-20 02:07:28 +0900

    병합연습

--- src/main/java/com/example/spring_study/GoalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring_study;
-
-import com.example.spring_study.Entity.Goal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface GoalRepository extends JpaRepository<Goal, Long> {
-}
--- src/main/java/com/example/spring_study/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring_study;
-
-import com.example.spring_study.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-interface UserRepository extends JpaRepository<User,Long> {
-}
