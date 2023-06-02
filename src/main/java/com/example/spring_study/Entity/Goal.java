cd842f87c0dae33ac45055f1a06c8536b81dee33
wooyong99 <wulovesk@naver.com>
2023-06-02 10:56:51 +0900

    <Fix> - 2차 피드백 수정

--- src/main/java/com/example/spring_study/Entity/Goal.java
+++ src/main/java/com/example/spring_study/Entity/Goal.java
@@ -16,9 +16,10 @@ public class Goal extends BaseEntity{    // 생성날짜 클래스 상속으로 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long goalId;   // 소대문자, 언더바 양식 통일
-    @Column(length = 32767, columnDefinition = "TEXT", nullable = false) // 글자 길이 수정
+    @Column(length = 32767, nullable = false) // 글자 길이 수정
     private String content;
     @ColumnDefault("false")
+    @Enumerated(EnumType.STRING)
     private boolean achieve;
     private LocalDateTime planDate;
     @ColumnDefault("0")
