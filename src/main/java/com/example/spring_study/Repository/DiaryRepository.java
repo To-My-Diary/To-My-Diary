86ac43d76bb39551d3f72e379855426039c535dc
soomin <smlee099@naver.com>
2023-06-16 13:31:14 +0900

    DiaryRepository 추가

--- src/main/java/com/example/spring_study/Repository/DiaryRepository.java
+++ src/main/java/com/example/spring_study/Repository/DiaryRepository.java
@@ -6,12 +6,15 @@ import com.example.spring_study.Enum.AchieveEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
     List<Diary> findByUser(User user);
 
-    List<Diary> findAllByAchieve(AchieveEnum achieve); //달성이라는 변수를 줄 때 
+    List<Diary> findAllByAchieve(AchieveEnum achieve); //achieve라는 기분으로 불러온것
+
+    List<Diary> findAllByCreateDate(LocalDate local); //날짜로 불러오기 위해서
 
 }
