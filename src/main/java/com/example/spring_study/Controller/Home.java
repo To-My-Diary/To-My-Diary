2d02150bd6acab2054cf155fc5715feac546ca2c
wooyong99 <wulovesk@naver.com>
2023-06-22 18:13:46 +0900

    클릭 날짜 받아오기

--- src/main/java/com/example/spring_study/Controller/Home.java
+++ src/main/java/com/example/spring_study/Controller/Home.java
@@ -3,11 +3,11 @@ package com.example.spring_study.Controller;
 import com.example.spring_study.Entity.Schedule;
 import com.example.spring_study.Repository.ScheduleRepository;
 import com.example.spring_study.Service.ScheduleService;
+import com.example.spring_study.VO.ClickDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,6 +17,13 @@ public class Home {
     private final ScheduleService scheduleService;
     @GetMapping(value = {"/","/index"})
     public String index(Model model){
+        List<Schedule> scheduleList=  scheduleService.getTodaySchedule();
+        model.addAttribute("scheduleList", scheduleList);
+        return "index";
+    }
+    @PostMapping(value = {"/","/index"})
+    public String index(@RequestBody ClickDate clickDate, Model model){
+        System.out.println(String.format("%s년%s월%s일", clickDate.getYear(), clickDate.getMonth(),clickDate.getDay()));
         List<Schedule> scheduleList=  scheduleService.getTodaySchedule();
         model.addAttribute("scheduleList", scheduleList);
         return "index";
---
+++ src/main/java/com/example/spring_study/VO/ClickDate.java
@@ -0,0 +1,11 @@
+package com.example.spring_study.VO;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class ClickDate {
+    String year;
+    String month;
+    String day;
+}
