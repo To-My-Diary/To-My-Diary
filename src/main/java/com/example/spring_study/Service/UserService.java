a2c6913f8d4ffd03d8642e822e7870a367e0bafa
wooyong99 <wulovesk@naver.com>
2023-10-29 15:57:09 +0900

    refactor : 회원가입 controller,service

--- src/main/java/com/example/spring_study/Service/UserService.java
+++ src/main/java/com/example/spring_study/Service/UserService.java
@@ -1,9 +1,16 @@
 package com.example.spring_study.Service;
 
+import static com.example.spring_study.DTO.Response.ResponseStatus.POST_EMAIL_DUPLICATE;
+import static com.example.spring_study.DTO.Response.ResponseStatus.POST_EMAIL_INCORRECT;
+import static com.example.spring_study.DTO.Response.ResponseStatus.POST_PASSWORD_INCORRECT;
+import static com.example.spring_study.DTO.Response.ResponseStatus.POST_TEL_DUPLICATE;
+import static com.example.spring_study.DTO.Response.ResponseStatus.SUCCESS;
+
 import com.example.spring_study.DTO.JoinDto;
 import com.example.spring_study.DTO.LoginDto;
+import com.example.spring_study.DTO.Response.ResponseDto;
 import com.example.spring_study.Entity.User;
-import com.example.spring_study.Exception.*;
+import com.example.spring_study.Exception.BaseException;
 import com.example.spring_study.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
@@ -12,19 +19,21 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import static com.example.spring_study.DTO.Response.ResponseStatus.*;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@EnableTransactionManagement
 public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public void create(JoinDto joinDto) {
-        if( !userRepository.findByEmail(joinDto.getEmail()).isEmpty() ){
+    @Transactional
+    public ResponseDto create(JoinDto joinDto) {
+        if (!userRepository.findByEmail(joinDto.getEmail()).isEmpty()) {
             throw new BaseException(POST_EMAIL_DUPLICATE);
-        }else if( !userRepository.findByTel(joinDto.getTel()).isEmpty()){
+        } else if (!userRepository.findByTel(joinDto.getTel()).isEmpty()) {
             throw new BaseException(POST_TEL_DUPLICATE);
         }
         User user = User.builder()
@@ -36,9 +45,10 @@ public class UserService {
                 .role("ROLE_USER")
                 .build();
         userRepository.save(user);
+        return new ResponseDto(SUCCESS);
     }
 
-    public User getUserByEmail(String user_email){
+    public User getUserByEmail(String user_email) {
         return userRepository.findByEmail(user_email).get();
     }
 
@@ -47,7 +57,7 @@ public class UserService {
                 () -> new BaseException(POST_EMAIL_INCORRECT)
         );
 
-        if(!bCryptPasswordEncoder.matches(loginDto.getPw(), user.getPw())){
+        if (!bCryptPasswordEncoder.matches(loginDto.getPw(), user.getPw())) {
             throw new BaseException(POST_PASSWORD_INCORRECT);
         }
         AbstractAuthenticationToken abstractAuthenticationToken = new UsernamePasswordAuthenticationToken(
--- src/main/java/com/example/spring_study/Util/UserValidation.java
+++ src/main/java/com/example/spring_study/Util/UserValidation.java
@@ -1,43 +1,59 @@
 package com.example.spring_study.Util;
 
-import com.example.spring_study.DTO.Response.ResponseDto;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
-
+import com.example.spring_study.DTO.JoinDto;
+import com.example.spring_study.DTO.Response.ResponseStatus;
+import com.example.spring_study.Exception.BaseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.springframework.validation.BindingResult;
 
 public class UserValidation {
-    public static boolean isRegexEmail(String target) {
+
+    private static boolean isRegexEmail(String target) {
         String regex = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$";
         Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(target);
         return matcher.find();
     }
-    public static boolean isRegexPw(String target) {
+
+    private static boolean isRegexPw(String target) {
         // 최소 8자리에 숫자, 문자, 특수 문자 각각 1개 이상 포함
         String regex = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$";
         Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(target);
         return matcher.find();
     }
-    public static boolean isRegexTel(String target) {
+
+    private static boolean isRegexTel(String target) {
         // 01[0,1,6~9] - 3,4자리(0~9) - 4자리(0~9) 형식이며, 하이푼을 포함
         String regex = "^01(?:0|1|[6-9])-[0-9]{3,4}-[0-9]{4}$";
         Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(target);
         return matcher.find();
     }
-    public static List<String> getValidationError(BindingResult bindingResult){
+
+    public static List<String> getValidationError(BindingResult bindingResult) {
         List<String> error_list = new ArrayList<>();
 
-        if(bindingResult.hasErrors()){  // Spring Validation을 이용하여 null, empty, 공백 검사
-            bindingResult.getFieldErrors().forEach(error->{
+        if (bindingResult.hasErrors()) {  // Spring Validation을 이용하여 null, empty, 공백 검사
+            bindingResult.getFieldErrors().forEach(error -> {
                 error_list.add(error.getDefaultMessage());
             });
         }
         return error_list;
+    }
+
+    public static void joinValidation(JoinDto joinDto) {
+        if (!UserValidation.isRegexEmail(joinDto.getEmail())) {  // 이메일 형식 오류
+            throw new BaseException(ResponseStatus.POST_EMAIL_INVALID);
+        } else if (!UserValidation.isRegexPw(joinDto.getPw())) {   // 비밀번호 형식 오류
+            throw new BaseException(ResponseStatus.POST_PASSSWORD_INVALID);
+        } else if (!UserValidation.isRegexTel(joinDto.getTel())) {    // 전화번호 형식 오류
+            throw new BaseException(ResponseStatus.POST_TEL_INVALID);
+        } else if (!joinDto.getPw().equals(joinDto.getConfirmPw())) {  // 비밀번호 확인 시 불일치 오류
+            throw new BaseException(ResponseStatus.POST_PASSWORD_DIFF);
+        }
     }
 }
